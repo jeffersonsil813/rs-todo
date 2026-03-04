4e2de5d5aaeb05b057075e73d2c1889ab160415e
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-03 21:20:21 -0300

    Add vite-plugin-svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/Trash-Regular.svg?react";
 
 const App = () => {
-  return <Text variant="body-sm-bold" className="text-pink-base">Hello World!</Text>;
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Hello World!
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <TrashIcon className="fill-pink-base" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
