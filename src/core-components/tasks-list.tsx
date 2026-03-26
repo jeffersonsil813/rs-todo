6559be7110af2245c4eb8109f1995ee9724c3a1a
Jefferson Santos da Silva <jefferson.sil813@gmail.com>
2026-03-25 23:07:57 -0300

    Add useTasks hook and update Taskslist and TasksSummary components for task management

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -1,18 +1,25 @@
-import Button from "../components/button";
 import PlusIcon from "../assets/icons/Plus-Regular.svg?react";
+import Button from "../components/button";
+import { useTasks } from "../hooks/use-tasks";
 import TaskItem from "./task-item";
 
 const Taskslist = () => {
-  return <>
-    <section>
-        <Button icon={PlusIcon} className="w-full">Nova Tarefa</Button>
-    </section>
-    <section className="space-y-2">
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
-    </section>
-  </>;
+  const { tasks } = useTasks();
+
+  return (
+    <>
+      <section>
+        <Button icon={PlusIcon} className="w-full">
+          Nova Tarefa
+        </Button>
+      </section>
+      <section className="space-y-2">
+        {tasks.map((task) => (
+          <TaskItem key={task.id} />
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default Taskslist;
--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,16 +1,25 @@
 import Badge from "../components/bagde";
 import Text from "../components/text";
+import { useTasks } from "../hooks/use-tasks";
 
 const TasksSummary = () => {
+  const { tasksCount, concludedTasksCount } = useTasks();
+
   return (
     <>
       <div className="flex items-center gap-2">
-        <Text variant="body-sm-bold" className="text-gray-300!">Tarefas criadas</Text>
-        <Badge variant="secondary">5</Badge>
+        <Text variant="body-sm-bold" className="text-gray-300!">
+          Tarefas criadas
+        </Text>
+        <Badge variant="secondary">{tasksCount}</Badge>
       </div>
       <div className="flex items-center gap-2">
-        <Text variant="body-sm-bold" className="text-gray-300!">Concluídas</Text>
-        <Badge>2 de 5</Badge>
+        <Text variant="body-sm-bold" className="text-gray-300!">
+          Concluídas
+        </Text>
+        <Badge>
+          {concludedTasksCount} de {tasksCount}
+        </Badge>
       </div>
     </>
   );
